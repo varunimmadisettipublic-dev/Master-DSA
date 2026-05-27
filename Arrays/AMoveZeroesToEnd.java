c16433a1e99cfbb496b4b3f2d18e31cda1ba0357
immadisetti-varun <varunimmadisettipublic@gmail.com>
2026-05-27 23:20:21 +0530

    Added readME file and started with first code arrays two pointers p1

---
+++ Arrays/AMoveZeroesToEnd.java
@@ -0,0 +1,26 @@
+/*
+
+We can use nested for loop and find i,j such that there sum gives target but it takes O(n^2) complexity
+When it comes to optimization we have Two Pointer approch, sliding window, prefixsum, kadane's algo
+Here Two Pointer approch makes more sense because goal is to find 2 indices.
+in addition to that it is mentioned that array is sorted. so if temp sum is high we decrement end else we increment start
+
+
+*/
+
+class AMoveZeroesToEnd {
+    public int[] twoSum(int[] nums, int target) {
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            if(nums[i]+nums[j]>target){
+                j--;
+            }else if(nums[i]+nums[j]<target){
+                i++;
+            }else{
+                break;
+            }
+        }
+        return new int[]{i+1, j+1};
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# You will get to see every problem I solve from (Today MAY 27 2026)
+As of Today I am an under confident person when it comes to solving DSA Questions
+I know Pattren wise learing makes much more sense and I am gonna do it in this
+# End goal is to make myself feel comfortable in solving problems
+Get Ready to give whatever it takes time, concentration and so on.
